b3d8fb16a26e3b6a9c73b09d0f503c72acff252f
dianaforrai <diana.forrai12@yahoo.com>
2024-10-20 10:08:11 +0300

    config swagger

--- server/app.js
+++ server/app.js
@@ -3,11 +3,35 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+// cors setup
+app.use(cors({
+  exposedHeaders: ['Authorization']
+}));
+
+// swagger setup
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Exchange API',
+      version: '0.1.0',
+    },
+  },
+  apis: ['./routes/*.js'],
+};
+
+const openapiSpecification = swaggerJsdoc(options);
+app.use('/api', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +47,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
