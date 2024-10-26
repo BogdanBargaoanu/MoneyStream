f545b97fd09bba4c9e6963a3780724aa54d3cc49
BogdanBargaoanu <bn19bba@yahoo.com>
2024-10-26 23:11:32 +0300

    Refactor partners route and add partner retrieval by username

--- server/routes/partners.js
+++ server/routes/partners.js
@@ -21,13 +21,15 @@ var router = express.Router();
  *                 idPartner:
  *                   type: integer
  *                 username:
- *                  type: string
+ *                   type: string
  *                 password:
- *                  type: string
+ *                   type: string
  *                 information:
- *                  type: string
+ *                   type: string
  *                 email:
- *                  type: string
+ *                   type: string
+ *                 success:
+ *                   type: boolean
  *       500:
  *         description: Internal server error.
  *         content:
@@ -37,6 +39,8 @@ var router = express.Router();
  *               properties:
  *                 error:
  *                   type: string
+ *                 success:
+ *                   type: boolean
  */
 
 /* GET partners listing. */
@@ -44,10 +48,82 @@ router.get('/', function (req, res, next) {
     const query = 'SELECT * FROM partner';
     req.db.query(query, (err, result) => {
       if (err) {
-        res.status(500).json({ error: err.message });
+        res.status(500).json({ error: err.message, success: false });
         return;
       }
-      res.json(result);
+      res.json({result, success: true });
+    });
+  });
+
+/**
+ * @openapi
+ * /partners/{name}:
+ *   get:
+ *     tags:
+ *      - partners
+ *     description: Gets the partner with the given partner username.
+ *     parameters:
+ *       - in: path
+ *         name: name
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Returns the partner.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 idPartner:
+ *                   type: integer
+ *                 username:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ *                 information:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       400:
+ *         description: Error caused by an inappropriate input.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ */
+
+router.get('/:username', function (req, res, next) {
+    const query = 'SELECT * FROM partner WHERE username = ?';
+    if (!req.params.username) {
+      res.status(400).json({ error: 'The request has missing information!' });
+      return;
+    }
+    req.db.query(query, [req.params.username], (err, result) => {
+      if (err) {
+        res.status(500).json({ error: err.message, success: false });
+        return;
+      }
+      res.json({result, success: true });
     });
   });
 
