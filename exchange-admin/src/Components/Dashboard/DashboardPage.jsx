9f5eec959b56dc2e0e2af0ab198fe99245907748
BogdanBargaoanu <bn19bba@yahoo.com>
2024-10-27 12:20:05 +0200

    Implemented basic navbar

--- exchange-admin/src/Components/Dashboard/DashboardPage.jsx
+++ exchange-admin/src/Components/Dashboard/DashboardPage.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar'
-import './Dashboard.css'
 import menu from '../Assets/main-menu.png'
+import './Dashboard.css'
 import logo from '../Assets/logo.png'
 
 const DashboardPage = () => {
-    const [showNav, setShowNav] = React.useState(false);
 
     return (
         <div>
-            <header>
-                <img className="menu" src={menu} alt="" onClick={() => setShowNav(!showNav)} />
-            </header>
+            <Navbar />
             <img className="logo" src={logo} alt="" />
-            {showNav && <Navbar />}
         </div>
     );
 }
