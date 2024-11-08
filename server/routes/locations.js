883d3aad57536e5d5fd77290dc7e6910279308c5
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-08 18:02:11 +0200

    Add locations route with JWT authentication and OpenAPI documentation and get endpoint

---
+++ server/routes/locations.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var router = express.Router();
+const jwt = require('jsonwebtoken');
+
+/**
+ * @openapi
+ * /locations:
+ *   get:
+ *     tags:
+ *      - location
+ *     description: Gets the list of locations.
+ *     responses:
+ *       200:
+ *         description: Returns the locations.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 idLocation:
+ *                   type: integer
+ *                 idPartner:
+ *                   type: integer
+ *                 address:
+ *                   type: string
+ *                 latitude:
+ *                   type: number
+ *                   format: double
+ *                 longitude:
+ *                   type: number
+ *                   format: double
+ *                 information:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ * components:
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+router.get('/', function (req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).json({ error: 'No authorization header', success: false });
+    return;
+  }
+
+  const token = authHeader.split(' ')[1]; // get the token from the Authorization header
+  let userId;
+  try {
+    const decoded = jwt.verify(token, 'exchange-secret-key'); // verify the token
+    userId = decoded.id; // get the partner ID from the decoded token
+  } catch (err) {
+    res.status(401).json({ error: 'Invalid token', success: false });
+    return;
+  }
+
+  const query = `SELECT * FROM location`;
+  req.db.query(query, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message, success: false });
+      return;
+    }
+    res.json({ result, success: true });
+  });
+});
+
+module.exports = router;
