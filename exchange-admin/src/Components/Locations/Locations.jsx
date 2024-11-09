7da0e34c83bda75435f38a09cea39ff4fe1f988a
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-09 14:32:20 +0200

    Add map display functionality with dynamic URL generation for location

--- exchange-admin/src/Components/Locations/Locations.jsx
+++ exchange-admin/src/Components/Locations/Locations.jsx
@@ -270,6 +270,12 @@ const Locations = () => {
             console.error('Geolocation is not supported by this browser.');
         }
     };
+    const generateMapUrl = (address) => {
+        if (address) {
+            return `https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=${encodeURIComponent(address)}&zoom=10&maptype=roadmap`;
+        }
+        return '';
+    };
 
     const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
         useTable({ columns, data });
@@ -360,6 +366,15 @@ const Locations = () => {
                             <button onClick={() => getCurrentLocation()} type="button" class="btn btn-primary" style={{ marginTop: 10 }}>
                                 Get current location
                             </button>
+                            <div className="mapswrapper">
+                                <iframe
+                                    width="100%"
+                                    height="300px"
+                                    loading="lazy"
+                                    allowFullScreen
+                                    src={generateMapUrl(currentLocation.address)}
+                                ></iframe>
+                            </div>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
