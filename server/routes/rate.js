28088b4824011e1e328095ec7a2e16a2a5272de0
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-15 18:47:15 +0200

    Insert rate endpoint config

--- server/routes/rate.js
+++ server/routes/rate.js
@@ -57,33 +57,151 @@ const jwt = require('jsonwebtoken');
  */
 
 router.get('/', function (req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    res.status(401).json({ error: 'No authorization header', success: false });
-    return;
-  }
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).json({ error: 'No authorization header', success: false });
+        return;
+    }
 
-  const token = authHeader.split(' ')[1]; // get the token from the Authorization header
-  let userId;
-  try {
-    const decoded = jwt.verify(token, 'exchange-secret-key'); // verify the token
-    userId = decoded.id; // get the partner ID from the decoded token
-  } catch (err) {
-    res.status(401).json({ error: 'Invalid token', success: false });
-    return;
-  }
+    const token = authHeader.split(' ')[1]; // get the token from the Authorization header
+    let userId;
+    try {
+        const decoded = jwt.verify(token, 'exchange-secret-key'); // verify the token
+        userId = decoded.id; // get the partner ID from the decoded token
+    } catch (err) {
+        res.status(401).json({ error: 'Invalid token', success: false });
+        return;
+    }
 
-  const query = `SELECT rate.idRates, rate.idLocation, location.address, rate.idCurrency, currency.name, rate.date, rate.value  FROM rate
+    const query = `SELECT rate.idRates, rate.idLocation, location.address, rate.idCurrency, currency.name, rate.date, rate.value  FROM rate
                     INNER JOIN location ON rate.idLocation = location.idLocation
                     INNER JOIN currency ON rate.idCurrency = currency.idCurrency
                     WHERE location.idPartner = ${userId}`;
-  req.db.query(query, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: err.message, success: false });
-      return;
+    req.db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message, success: false });
+            return;
+        }
+        res.json({ result, success: true });
+    });
+});
+
+/**
+ * @openapi
+ * /rate/insert:
+ *   post:
+ *     tags:
+ *      - rate
+ *     description: Add a rate.
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               idLocation:
+ *                 type: integer
+ *                 description: Foreign key of the location.
+ *               idCurrency:
+ *                 type: integer
+ *                 description: Foreign key of the currency.
+ *               date:
+ *                 type: string
+ *                 format: date-time
+ *               value:
+ *                 type: number
+ *                 format: double
+ *     responses:
+ *       200:
+ *         description: Rate added successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       400:
+ *         description: Error caused by an inappropriate input.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ * components:
+ *   securitySchemes:
+ *     BearerAuth:
+ *       type: http
+ *       scheme: bearer
+ *       bearerFormat: JWT
+ */
+
+router.post('/insert', function (req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).json({ error: 'No authorization header', success: false });
+        return;
     }
-    res.json({ result, success: true });
-  });
+
+    const token = authHeader.split(' ')[1]; // get the token from the Authorization header
+    let userId;
+    try {
+        const decoded = jwt.verify(token, 'exchange-secret-key'); // verify the token
+        userId = decoded.id; // get the partner ID from the decoded token
+    } catch (err) {
+        res.status(401).json({ error: 'Invalid token', success: false });
+        return;
+    }
+
+    const { idLocation, idCurrency, date, value } = req.body;
+    const insertQuery = 'INSERT INTO rate (idLocation, idCurrency, date, value) VALUES (?, ?, ?, ?)';
+
+    console.log(req.body);
+    if (!idLocation || !idCurrency || !date || !value) {
+        res.status(400).json({ error: 'The request has missing information!', success: false });
+        return;
+    }
+    const formattedDate = new Date(date).toISOString().slice(0, 19).replace('T', ' ');
+    req.db.beginTransaction((err) => {
+        if (err) {
+            res.status(500).json({ error: err.message, success: false });
+            return;
+        }
+        req.db.query(insertQuery, [idLocation, idCurrency, formattedDate, value], (err, result) => {
+            if (err) {
+                res.status(500).json({ error: err.message, success: false });
+                return;
+            }
+            req.db.commit((err) => {
+                if (err) {
+                    return req.db.rollback(() => {
+                        res.status(500).json({ error: err.message, success: false });
+                    });
+                }
+                res.json({ message: 'Rate added successfully!', success: true });
+            });
+        });
+    });
 });
 
 module.exports = router;
