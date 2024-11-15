6a8e77b232dcb0c36db408bb43c10858afeac0f8
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-15 17:40:39 +0200

    Refactor button styles and actions layout; consolidate delete button styles into App.css and update Currency and Locations components

--- exchange-admin/src/Components/Locations/Locations.jsx
+++ exchange-admin/src/Components/Locations/Locations.jsx
@@ -97,7 +97,7 @@ const Locations = () => {
             {
                 Header: "Actions",
                 Cell: ({ row }) => (
-                    <>
+                    <div className='actions-container'>
                         <button onClick={() => handleUpdate(row.original)} type="button" className="btn btn-primary btn-update" data-bs-toggle="modal" data-bs-target="#modal-location">
                             Update
                         </button>
@@ -109,7 +109,7 @@ const Locations = () => {
                                     d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                             </svg>
                         </button>
-                    </>
+                    </div>
                 ),
             }
         ],
