48358171cc1569c1d725175dbffe699b56ae644a
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-17 00:33:11 +0200

    Add Roboto font, refactor Home component styles, and enhance button options

--- exchange-client/src/Components/Home/Home.jsx
+++ exchange-client/src/Components/Home/Home.jsx
@@ -3,13 +3,15 @@ import './Home.css';
 import logo from '../Assets/logo.png';
 
 const Home = () => {
-  return (
-    <div className='container-main'>
-    <img className='logo' src={logo} alt='logo' />
-      <h1 className='heading-main'>Exchange Monitor</h1>
-    <button className='btn-main'>Get nearest rates</button>
-    </div>
-  );
+    return (
+        <div className='container-main'>
+            <img className='logo' src={logo} alt='logo' />
+            <h1 className='heading-main'>Exchange Monitor</h1>
+            <button className='btn-main'>Nearest rates</button>
+            <button className='btn-main'>Best rates</button>
+            <button className='btn-main'>All rates</button>
+        </div>
+    );
 }
 
 export default Home;
