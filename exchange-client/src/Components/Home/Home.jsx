1c223bea990c4ce84fca9b0f91b3fbf72ae2e8a7
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-17 00:50:39 +0200

    Add react-icons package, enhance Home component with icons, and improve responsive styles

--- exchange-client/src/Components/Home/Home.jsx
+++ exchange-client/src/Components/Home/Home.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './Home.css';
 import logo from '../Assets/logo.png';
+import { PiListBulletsBold } from "react-icons/pi";
+import { MdOutlineVerified } from "react-icons/md";
+import { ImLocation } from "react-icons/im";
+
 
 const Home = () => {
     return (
         <div className='container-main'>
             <img className='logo' src={logo} alt='logo' />
             <h1 className='heading-main'>Exchange Monitor</h1>
-            <button className='btn-main'>Nearest rates</button>
-            <button className='btn-main'>Best rates</button>
-            <button className='btn-main'>All rates</button>
+            <button className='btn-main'><ImLocation /> Nearest rates</button>
+            <button className='btn-main'><MdOutlineVerified /> Best rates</button>
+            <button className='btn-main'><PiListBulletsBold /> All rates</button>
         </div>
     );
 }
