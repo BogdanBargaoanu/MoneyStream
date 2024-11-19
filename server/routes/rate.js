bce4ddaa5475c886808c7bc412cc93ea5bccb7fd
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-19 23:00:18 +0200

    Add API endpoint to retrieve all rates with OpenAPI documentation for client use.

--- server/routes/rate.js
+++ server/routes/rate.js
@@ -73,7 +73,7 @@ router.get('/', function (req, res, next) {
         return;
     }
 
-    const query = `SELECT rate.idRates, rate.idLocation, location.address, rate.idCurrency, currency.name, rate.date, rate.value  FROM rate
+    const query = `SELECT rate.idRates, rate.idLocation, location.address, rate.idCurrency, currency.name, rate.date, rate.value FROM rate
                     INNER JOIN location ON rate.idLocation = location.idLocation
                     INNER JOIN currency ON rate.idCurrency = currency.idCurrency
                     WHERE location.idPartner = ${userId}`;
@@ -530,6 +530,66 @@ router.get('/nearest', function (req, res, next) {
             return;
         }
         res.json({ result, page: page, success: true });
+    });
+});
+
+/**
+ * @openapi
+ * /rate/allRates:
+ *   get:
+ *     tags:
+ *      - rate
+ *     description: Gets the list of rates for public use.
+ *     responses:
+ *       200:
+ *         description: Returns the rates.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 idRates:
+ *                   type: integer
+ *                 idLocation:
+ *                   type: integer
+ *                 address:
+ *                   type: string
+ *                 idCurrency:
+ *                   type: integer
+ *                 name:
+ *                   type: string
+ *                 date:
+ *                   type: string
+ *                   format: date-time
+ *                 value:
+ *                   type: number
+ *                   format: double
+ *                 success:
+ *                   type: boolean
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                 success:
+ *                   type: boolean
+ */
+
+router.get('/allRates', function (req, res, next) {
+
+    const query = `SELECT rate.idRates, rate.idLocation, location.address, rate.idCurrency, currency.name, rate.date, rate.value FROM rate
+                    INNER JOIN location ON rate.idLocation = location.idLocation
+                    INNER JOIN currency ON rate.idCurrency = currency.idCurrency`;
+    req.db.query(query, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message, success: false });
+            return;
+        }
+        res.json({ result, success: true });
     });
 });
 
