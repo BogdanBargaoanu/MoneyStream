3e76f9ed5718837102fb516e2d7c4563dec680eb
BogdanBargaoanu <bn19bba@yahoo.com>
2024-11-28 21:44:08 +0200

    Add rendering logic for grouped rates with map integration and load more button

--- exchange-client/src/Components/BestRates/BestRates.jsx
+++ exchange-client/src/Components/BestRates/BestRates.jsx
@@ -15,6 +15,36 @@ const BestRates = () => {
         <div className='container-best-rates'>
             <MdOutlineArrowBack className='home-button' onClick={navigateHome} />
             <h1 className='heading-best-rates'><MdOutlineVerified /> Best Rates</h1>
+            <div className='data-container'>
+                {Object.keys(groupedRates).length > 0 ? (
+                    <ul>
+                        {Object.entries(groupedRates).map(([address, rates], index) => (
+                            <div className='container-rate' key={index}>
+                                <h3>Address: {address}</h3>
+                                <iframe
+                                    width="80%"
+                                    height="300px"
+                                    loading="lazy"
+                                    allowFullScreen
+                                    src={generateMapUrlFromAddress(address)}
+                                ></iframe>
+                                <ul>
+                                    {rates.map((rate, idx) => (
+                                        <li key={idx}>
+                                            <p>Currency: {rate.name}</p>
+                                            <p>Rate: {rate.value}</p>
+                                            <p>Date: {new Date(rate.date).toLocaleDateString()}</p>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>No rates found.</p>
+                )}
+            </div>
+            <button className='load-more' onClick={loadNewData}>Load More</button>
         </div>
     );
 }
